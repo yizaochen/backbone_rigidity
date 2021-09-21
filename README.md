3064e54d5955391116c5319aa6e0c60ba09f6add
yizaochen <yizaochen@gmail.com>
2021-09-21 17:47:19 +0800

    Update

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# backbone_rigidity
+# Backbone-Rigidity
+
+# Create a virtual environment
+`conda create --name backbone python=3.8`
+
+# Activate virtual environment
+`conda activate backbone`
+
+# Deactivate
+`conda deactivate`
+
+# Install Backbone-Rigidity package
+`pip install -e .`
+
+# Upgrade package
+`pip install -e . --upgrade`
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(name='backbone', 
+      version='0.1',
+      packages=find_packages(),
+      url='https://github.com/yizaochen/backbone_rigidity.git',
+      author='Yizao Chen',
+      author_email='yizaochen@gmail.com',
+      license='MIT',
+      install_requires=[
+          'jupyterlab',
+          'pandas',
+          'numpy'
+      ]
+      )
